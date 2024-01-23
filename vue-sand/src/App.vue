913363ec26b0187c48d3098bcdac3a12b8f7a209
Forsrobin <forsrobin@gmail.com>
2024-01-23 22:49:16 +0100

    Add initial project files and configurations for Vue

---
+++ vue-sand/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+// imports go here
+</script>
+
+<template>
+  <header>
+    <h1>Vue Sand</h1>
+  </header>
+
+  <main>
+    <p>Test</p>
+  </main>
+</template>
+
+<style scoped>
+* {
+  min-width: 0px;
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+</style>
