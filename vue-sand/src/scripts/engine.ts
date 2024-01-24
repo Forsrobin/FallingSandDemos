8a438f8c3c4846e1b34f6c742274deeb836def90
Forsrobin <forsrobin@gmail.com>
2024-01-24 01:16:00 +0100

    Robin: Created psudo code for update and commented code

--- vue-sand/src/scripts/engine.ts
+++ vue-sand/src/scripts/engine.ts
@@ -1,19 +1,22 @@
+// Default canvas attributes
 const WIDTH = 500
 const HEIGHT = 500
 
+// Dedfault pixel attributes
 const PIXEL_WIDTH = 10
 const PIXEL_HEIGHT = 10
-const PIXEL_COLOR = 'yellow'
 
+// Global variables
 let ctx: CanvasRenderingContext2D | null
 let canvasArray: number[][] = [[]]
-
 let then = Date.now();
 let now = Date.now();
 let delta: number
 let fps = 240;
 let interval = 1000 / fps;
 
+// Init canvas
+// Used to initialize the canvas element with the correct attributes
 const initCanvas = () => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement
     canvas.width = WIDTH
@@ -22,18 +25,23 @@ const initCanvas = () => {
     return canvas
 }
 
+// Draw a pixel at a specific coordinate based on the pixel attributes
 const drawPixelCoordinate = (x: number, y: number, color: string) => {
     if (!ctx) return
     ctx.fillStyle = color
-    ctx.fillRect(x, y, PIXEL_WIDTH, PIXEL_HEIGHT)
+    ctx.fillRect(x, y, PIXEL_WIDTH, PIXEL_HEIGHT) // x, y, width, height
 }
 
+// Returns a cleared 2D array with only 0's
+// This is used to store states and create new states 
+// for the updated canvas
 const clearCanvasArray = () => {
     let array: number[][] = [[]]
     let xCounter = Math.floor(WIDTH / PIXEL_WIDTH)
     let yCounter = Math.floor(HEIGHT / PIXEL_HEIGHT)
+
     for (let row = 0; row < xCounter; row++) {
-        array[row] = []
+        array[row] = [] // Make sure we have an array at each row, otherwise we can't push values there
         for (let column = 0; column < yCounter; column++) {
             array[row][column] = 0
         }
@@ -41,17 +49,42 @@ const clearCanvasArray = () => {
     return array
 }
 
+// Draw function
+// Used to draw ther pixels on the canvas
+// based on the canvasArray and given attributes
 const draw = () => {
     for (let row = 0; row < canvasArray.length; row++) {
         for (let column = 0; column < canvasArray[row].length; column++) {
             if (canvasArray[row][column] == 1) {
-                drawPixelCoordinate(row * PIXEL_WIDTH, column * PIXEL_HEIGHT, "red")
+                drawPixelCoordinate(row * PIXEL_WIDTH, column * PIXEL_HEIGHT, "yellow")
             }
         }
     }
 }
 
+// Update function
+// Used to update the canvasArray and create a new state
 const update = () => {
+
+    // Psuedo code
+
+    // Create a new empty canvasArray
+    // Loop through canvasArray
+    //      If pixel is empty: 
+    //          continue
+    //      If pixel is a falling entity (Sand, Water, etc.):
+    //          If pixel below is occupied:
+    //              Set newEmptyCanvasArray at pixel as falling entity (Dont move basically)
+    //              continue
+    //          If pixel below is empty (not a falling entity):
+    //              Set newEmptyCanvasArray at pixel below as the falling entity
+    //              continue
+    //          If pixel below is sand AND random pixel diagonal below is empty (Diagonal left or right):
+    //              Set newEmptyCanvasArray at pixel diagonal below as falling entity
+    //              continue
+    // Set the canvasArray to the newEmptyCanvasArray
+
+
     for (let row = 0; row < canvasArray.length; row++) {
         for (let column = 0; column < canvasArray[row].length; column++) {
             //Check if pixel is Sand
@@ -88,8 +121,8 @@ export const initEngine = () => {
     canvasArray = clearCanvasArray()
 
     if (ctx != null) {
+        // Inital pixel to test
         canvasArray[25][5] = 1
-        // drawPIXELAtCoordinates(ctx, 0, 0)
     }
 
     gameLoop()
